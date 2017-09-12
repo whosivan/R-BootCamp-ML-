8c74880e77e6a3c3866c1b5dbb926f36675d388b
Ivan Cui <ivancjr@uw.edu>
2017-09-11 20:10:40 -0700

    initial commit saving file

---
+++ R-Course-HTML-Notes-Training-ex_codes/Logical operation.R
@@ -0,0 +1,30 @@
+#---------LOGICAL OPERATORS
+x <- 10
+x < 20
+x > 5
+(x < 20) & (x > 5) #multiple logical operation, using & requires all statements to be TRUE
+
+x == 100
+x == 10
+(x == 10) | (x == 100) #OR statement, need at least one statement to be TRUE
+
+!(10 == 1)#NOT statement
+
+df <- mtcars
+head(mtcars)
+df[df$mpg > 20, 'mpg']
+
+subset(df, mpg > 20)
+
+df[(df$mpg > 20) & (df$hp > 100), ]
+df[(df$mpg > 20) | (df$hp > 100), ]
+
+#-------------If, else, and elif
+x <- 13
+if(x == 10){
+  print('x is equal to 10!')
+}else if(x == 12){
+  print('x equal to 12')
+}else{
+  print('x was not equal to 10 or 12')
+}
