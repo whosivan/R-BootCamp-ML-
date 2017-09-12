558c55e69075f5d2a3f886c8e1ea3550cdca88fb
Ivan Cui <ivancjr@uw.edu>
2017-09-11 20:21:26 -0700

    initial commit saving file

--- R-Course-HTML-Notes-Training-ex_codes/Logical operation.R
+++ R-Course-HTML-Notes-Training-ex_codes/Logical operation.R
@@ -28,3 +28,17 @@ if(x == 10){
 }else{
   print('x was not equal to 10 or 12')
 }
+
+ham <- 10
+cheese <- 10
+report <- 'blank'
+
+if(ham >= 10 & cheese >= 10){
+ report <- 'Strong sales of both ham and cheese' 
+}else if(ham == 0 & cheese ==0){
+  report <- 'No sales today!'
+}else{
+  report <- 'we sold something today!'
+}
+
+report
