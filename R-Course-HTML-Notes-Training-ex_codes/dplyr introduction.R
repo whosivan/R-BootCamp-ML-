67c49d9489ca342d7001d3420f2adca43dbf5ce1
Ivan Cui <ivancjr@uw.edu>
2017-09-13 11:49:05 -0700

    training exer completed

--- R-Course-HTML-Notes-Training-ex_codes/dplyr introduction.R
+++ R-Course-HTML-Notes-Training-ex_codes/dplyr introduction.R
@@ -34,3 +34,28 @@ summarise(flights, avg_air_time = mean(air_time, na.rm = TRUE))
 sample_n(flights, 10)
 #sample_facr()
 sample_frac(flights, 0.3)
+
+#1.
+library(dplyr)
+head(mtcars)
+filter(mtcars, mpg > 20, cyl == 6)
+
+#2.
+arrange(mtcars, cyl, desc(wt))
+
+#3.
+select(mtcars, mpg, hp)
+
+#4.
+distinct(select(mtcars, gear))
+
+#5.
+mutate(mtcars, Performance = hp/wt)
+
+#6.
+summarise(mtcars, avg_mpg = mean(mpg, na.rm = TRUE))
+
+#7.
+mtcars %>%
+  filter(cyl == 6) %>%
+  summarise(avg_hp = mean(hp))
